3dc2d95a74b56ea6a111ea5e806f17b2ffd16c44
vendascorrreia-hue <vendascorrreia@gmail.com>
2025-11-21 21:14:31 -0300

    Add files via upload
    versão de lançamento

--- kshell.py
+++ kshell.py
@@ -1,20 +1,20 @@
 import os
 import webbrowser
-def sistema(a, e, au):
+def sistema(a, e, au, poweroff, reboot,navegador):
     os.system("clear")
     while True:
         os.system("clear")
         print ("ai embaixo está oque vocẽ pode fazer, divirta-se com o kshell!!")
-        print ("1. editor de texto   2. terminal77   3. loja   4. lynx")
+        print (f"1. editor de texto   2. terminal77   3. loja   4. {navegador}")
         menu = input ("selecione uma opção(ou digite rbt para reiniciar e dsl para desligar) ")
         if "dsl" in menu:
-            os.system("shutdown now")
+            os.system(f"{poweroff}")
         elif "rbt" in menu:
-            os.system("reboot")
+            os.system(f"{reboot}")
         elif "1" in menu:
             os.system(f"{e} ")
         elif "4" in menu:
-            os.system("lynx")
+            os.system(f"{navegador}")
         elif "3" in menu:
             while True:
                 os.system("clear")
@@ -68,10 +68,11 @@ def sistema(a, e, au):
             input()
 os.system("clear")
 print ("KSH >_< ELL")
-print ("olá, poderia nos dizer qual é a sua distro (ou baseada em alguma dessas)")
+print ("olá, poderia nos dizer qual é o seu sistema (ou baseado em algum desses)")
 print ("1. archlinux")
 print ("2. ubuntu")
 print ("3. Freebsd")
+print ("4. Windows")
 while True:
     p = input (": ")
     if "1" in p:
@@ -84,13 +85,13 @@ while True:
                 os.system(f"sudo pacman -S  --noconfirm {editor} {navegador}")
                 os.system("clear")
                 input ("otimo, pressione enter para começar a usar o sistema")
-                sistema("sudo pacman -S --noconfirm",editor, "sudo pacman -Rns --noconfirm")
+                sistema("sudo pacman -S --noconfirm",editor, "sudo pacman -Rns --noconfirm","shutdown now","reboot","lynx")
            elif "2" in boot:
                editor = input ("qual é seu editor de texto? ")
-               sistema("sudo pacman -S --noconfirm",editor, "sudo pacman -Rns --noconfirm")
+               sistema("sudo pacman -S --noconfirm",editor, "sudo pacman -Rns --noconfirm","shutdown now","reboot","lynx")
     elif "2" in p:
         editor = input ("qual é seu editor de texto? ")
-        sistema("sudo apt install -y",editor, "sudo apt remove -y")
+        sistema("sudo apt install -y",editor, "sudo apt remove -y","shutdown now","reboot","lynx")
     elif "3" in p:
         print ("você tem uma instalação simples ou completa(com navegador e etc...)? digite 1 para simples e 2 para completa")
         while True:
@@ -101,11 +102,14 @@ while True:
                 os.system(f"sudo pacman -S  --noconfirm {editor} {navegador}")
                 os.system("clear")
                 input ("otimo, pressione enter para começar a usar o sistema")
-                sistema("pkg install",editor, "pkg install")
+                sistema("pkg install",editor, "pkg install","poweroff","reboot","lynx")
            elif "2" in boot:
                editor = input ("qual é seu editor de texto? ")
-               sistema("pkg install",editor, "pkg install")
-
+               sistema("pkg install",editor, "pkg install","poweroff","reboot","lynx")
+    elif "4" in p:
+        editor = input ("editor de texto: ")
+        navegado = input ("navegador: ")
+        sistema("winget install",editor, "winget uninstall","shutdown /s /t 00","shutdown /r /t 0",navegado)
 
 
 
